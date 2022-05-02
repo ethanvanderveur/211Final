112f2c93d15dcaf3983f3595ded0d93ae995c6b8
JonahCraven <92745856+JonahCraven@users.noreply.github.com>
2022-05-02 15:23:34 -0700

    turret work

---
+++ 211Final/Assets/Coding/Scripts/Turret.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public LayerMask layer;
+    [SerializeField]
+    bool lineOfSight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineOfSight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rayDirection;
+        GameObject target;
+        target = GameObject.FindGameObjectWithTag("PlayerCharacter");
+        rayDirection = target.transform.position - transform.position;
+        var ray = new Ray(this.transform.position, rayDirection);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 40, layer)) {
+            lineOfSight = false;
+        }
+        else {
+            lineOfSight = true;
+        }
+
+
+        if (lineOfSight) {
+            //this.transform.rotation = Quaternion.Euler(rayDirection);
+            this.GetComponent<GameObject>().transform.LookAt(target.transform);
+        }
+    }
+}
